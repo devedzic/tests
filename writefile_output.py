cc40f72ded1e4347030fdc8fcb9af45376916799
devedzic <devedzic@gmail.com>
2022-09-13 23:39:10 +0200

    Testing import, debugger and magic commands.

---
+++ writefile_output.py
@@ -0,0 +1,16 @@
+'''Testing %%writefile
+'''
+
+
+def return_1():
+    '''A simple function
+    '''
+    
+    return 1
+
+
+def return_2():
+    '''Another simple function
+    '''
+    
+    return 2
